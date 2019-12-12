a0806c700ad8ea22bab1f27be8adc13fba419665
Victor Ribeiro <victorribeiro@teksystem.com.br>
2019-12-12 08:28:57 -0300

    add coolmap

--- js/main.js
+++ js/main.js
@@ -12,14 +12,44 @@ let buffer2 = Array(w).fill().map(_=>Array(h).fill(0));
 let tmpCanvas = canvas.cloneNode(true),
 		tmpC = tmpCanvas.getContext('2d');
 
-tmpC.font = "30px Arial";
-tmpC.textAlign = "center"
-tmpC.fillRect(0,0,w,h)
-tmpC.fillStyle = "rgb(50,50,50)"
-tmpC.fillText("Hello World", w/2, h);
+let coolmap;
+
+const img = new Image()
+img.src = './img/coolmap2.png'
+img.crossOrigin = "Anonymous"
+img.onload = _ => {
+	
+	//let pat = tmpC.createPattern(img, "repeat");
+	//tmpC.fillStyle = pat
+	//tmpC.fillRect(0,0,w,h)
+	tmpC.drawImage(img,0,0,w,h)
+	let _coolmap = tmpC.getImageData(0,0,w,h);
+	coolmap = Array(w).fill().map(_=>Array(h).fill(0));
+	for(let i = 0; i < coolmap.length; i++){
+		for(let j = 0; j < coolmap[0].length; j++){
+			let index = (j * buffer1.length + i) * 4
+			coolmap[i][j] = _coolmap.data[index] * 0.2126 +
+											_coolmap.data[index+1] * 0.7152 +
+											_coolmap.data[index+2] * 0.0722
+		}
+	}
+	tmpC.font = "60px Arial";
+	tmpC.textAlign = "center"
+	tmpC.fillRect(0,0,w,h)
+	tmpC.fillStyle = "white"
+	tmpC.fillText("Hello World", w/2, h/2);
+	animation()
+}
+
+
+
+
+
 
 
 let temp;
+
+scroll = 0;
 
 function animation(){
 	
@@ -31,24 +61,28 @@ function animation(){
 					n3 = buffer1[i][j+1],
 					n4 = buffer1[i][j-1];
 			
-			buffer2[i][j-1] = Math.max((n1+n2+n3+n4)/4,0)
+			buffer2[i][j-1] = Math.max(((n1+n2+n3+n4)/4)-(coolmap[i][(j+scroll)%h]*0.1),0)
+			
+			
 			
 		}
 	}
 	
-	let img = new ImageData(w, h)
+	scroll+=2
+	
+	let _img = new ImageData(w, h)
 	
 	for(let i = 0; i < buffer1.length; i++){
 		for(let j = 0; j < buffer1[0].length; j++){
 			let index = (j * buffer1.length + i) * 4
-			img.data[index] = buffer2[i][j]
-			img.data[index+1] = buffer2[i][j]
-			img.data[index+2] = buffer2[i][j]
-			img.data[index+3] = 255
+			_img.data[index] = buffer2[i][j]
+			_img.data[index+1] = buffer2[i][j]
+			_img.data[index+2] = buffer2[i][j]
+			_img.data[index+3] = 255
 		}
 	}
 	
-	c.putImageData(img,0,0)
+	c.putImageData(_img,0,0)
 	
 	temp = buffer2;
 	buffer2 = buffer1;
@@ -57,14 +91,12 @@ function animation(){
 	ripple()
 }
 
-animation();
-
 function ripple(e){
 	let img = tmpC.getImageData(0,0,w,h);
 	
 	for(let i = 0; i < buffer1.length; i++){
 		for(let j = 0; j < buffer1[0].length; j++){
-			if(img.data[(j * buffer1.length + i) * 4])
+			if(img.data[(j * buffer1.length + i) * 4] > 240)
 				buffer1[i][j] = img.data[(j * buffer1.length + i) * 4]
 		}
 	}
@@ -74,6 +106,8 @@ document.addEventListener("click", ripple  );
 
 //document.addEventListener("mousemove", ripple  );
 
+/*
 window.addEventListener("resize",function(){
 	location.reload();
 });
+*/
