55dce4098c040f086cabdb1a22b91ff1a061c40f
Victor Ribeiro <victorqribeiro@gmail.com>
2020-08-30 23:49:38 -0300

    improve effect

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
-const s = 1; /* size - the bigger the faster (lower quality) */
-const canvas = document.getElementById("canvas");
-canvas.width = w = Math.floor(innerWidth/s);
-canvas.height = h = Math.floor(innerHeight/s);
-canvas.style.width = '100%';
-canvas.style.height = '100%';
-const c = canvas.getContext("2d");
+const s = 1 /* size - the bigger the faster (lower quality) */
+const canvas = document.getElementById("canvas")
+canvas.width = w = Math.floor(innerWidth/s)
+canvas.height = h = Math.floor(innerHeight/s)
+canvas.style.width = '100%'
+canvas.style.height = '100%'
+const c = canvas.getContext("2d")
 
-let buffer1 = Array(w).fill().map(_=>Array(h).fill(0));
-let buffer2 = Array(w).fill().map(_=>Array(h).fill(0));
+let buffer1 = Array(w).fill().map(_=>Array(h).fill(0))
+let buffer2 = Array(w).fill().map(_=>Array(h).fill(0))
 
 let tmpCanvas = canvas.cloneNode(true),
-		tmpC = tmpCanvas.getContext('2d');
-
-let coolmap;
+    tmpC = tmpCanvas.getContext('2d'),
+    coolmap, textData, scroll = 0, d = []
 
 const img = new Image()
 img.src = './img/coolmap.png'
@@ -25,42 +24,44 @@ img.onload = _ => {
 		for(let j = 0; j < coolmap[0].length; j++){
 			let index = (j * buffer1.length + i) * 4
 			coolmap[i][j] = _coolmap.data[index] * 0.2126 +
-											_coolmap.data[index+1] * 0.7152 +
-											_coolmap.data[index+2] * 0.0722
+						    _coolmap.data[index+1] * 0.7152 +
+						    _coolmap.data[index+2] * 0.0722
 		}
 	}
 	let text = getMessage() || "Hello World!"
-	let fontSize = w / (text.length > 1 ? text.length : 2 );
-	tmpC.font = fontSize+"px Arial";
+	let fontSize = w / (text.length > 1 ? text.length : 2 )
+	tmpC.font = fontSize+"px Verdana";
 	tmpC.textAlign = "center"
 	tmpC.fillRect(0,0,w,h)
 	tmpC.fillStyle = "white"
-	tmpC.fillText(text, w/2, h/2 + fontSize/3);
+	tmpC.fillText(text, w/2, h/2 + fontSize/3)
+	textData = tmpC.getImageData(0,0,w,h)
+	for(let i = 0; i < buffer1.length; i++){
+		for(let j = 0; j < buffer1[0].length; j++){
+			if(textData.data[(j * buffer1.length + i) * 4] > 240)
+				d.push(i,j)
+		}
+	}
 	animation()
 }
 
-scroll = 0;
-
-function getMessage() {
+const getMessage = () => {
 	return (new URL(document.location)).searchParams.get('message')
 }
 
-function animation(){
+const animation = () => {
 	
 	for(let i = 1; i < w-2; i++){
 		for(let j = 1; j < h-2; j++){
-
 			let n1 = buffer1[i+1][j],
-					n2 = buffer1[i-1][j],
-					n3 = buffer1[i][j+1],
-					n4 = buffer1[i][j-1];
-			
+				n2 = buffer1[i-1][j],
+				n3 = buffer1[i][j+1],
+				n4 = buffer1[i][j-1]
 			buffer2[i][j-1] = Math.max(((n1+n2+n3+n4)/4)-(coolmap[i][(j+scroll)%h]*0.1),0)
-			
 		}
 	}
 	
-	scroll+=2
+	scroll += 2
 	
 	let _img = new ImageData(w, h)
 	
@@ -76,26 +77,16 @@ function animation(){
 	
 	c.putImageData(_img,0,0)
 	
-	let temp = buffer2;
-	buffer2 = buffer1;
-	buffer1 = temp;
-	requestAnimationFrame(animation);
-	ripple()
+	let temp = buffer2
+	buffer2 = buffer1
+	buffer1 = temp
+	requestAnimationFrame(animation)
+	ignite()
 }
 
-function ripple(){
-	let img = tmpC.getImageData(0,0,w,h);
-	
-	for(let i = 0; i < buffer1.length; i++){
-		for(let j = 0; j < buffer1[0].length; j++){
-			if(img.data[(j * buffer1.length + i) * 4] > 240)
-				buffer1[i][j] = img.data[(j * buffer1.length + i) * 4]
-		}
-	}
+const ignite = () => {
+    for(let i = 0; i < d.length; i+=2)
+        buffer1[d[i]][d[i+1]+Math.floor(Math.random()*10-20)] += 12
 }
 
-
-window.addEventListener("resize",function(){
-	location.reload();
-});
-
+window.addEventListener("resize", ()=>{ location.reload() })
